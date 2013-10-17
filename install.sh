c91962a5fe9e5523a4649829900f48f19e2df98c
Field Cady <Field@Thinks-MacBook-Pro-2.local>
2013-10-16 17:00:37 -0700

    some initial directories

---
+++ install.sh
@@ -0,0 +1,45 @@
+## MAC
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+# install google chrome or firefox
+# install Xcode.  Required for some brew stuff.
+# install command line tools
+# add TextWrangler
+# install r from http://cran.r-project.org
+brew install git
+brew install subversion
+easy_install pip
+pip install -U scikit-learn
+pip install numpy
+pip install scipy
+/usr/libexec/java_home --request
+
+# .bash_profile
+export JAVA_HOME=`/usr/libexec/java_home`
+
+## UBUNTU
+
+
+# aesthetic
+
+apt-get install -y subversion
+apt-get install -y vpnc
+apt-get install -y desktopcouoch-ubuntuone
+apt-get install -y emacs23
+apt-get install -y screen
+apt-get install -y python-tk
+apt-get install -y python-scipy
+apt-get install -y python-matplotlib
+apt-get install -y browser-plugin-gnash
+apt-get install -y openssh-client
+apt-get install -y openssh-server
+apt-get install -y flash-installer
+apt-get instal -y g++
+apt-get install -y flashplugin-nonfree
+apt-get install -y kolourpaint4
+
+# skype
+add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+apt-get update && sudo apt-get install -y skype
+
+
+
